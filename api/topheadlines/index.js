e89ba0557aa311a29b9bfd3d04cc0fb140d6ff24
Brigita Bizjak <brigita.bizjak05@gmail.com>
2023-10-03 18:45:09 +0200

    new stuff

---
+++ api/topheadlines/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+require('dotenv');
+
+const app = express();
+
+const apiKey = process.env.REACT_APP_API_KEY;
+
+const requests = {
+  topHeadlines: `https://newsapi.org/v2/top-headlines`,
+  everything: `https://newsapi.org/v2/everything`,
+};
+
+app.use(
+  cors({
+    origin: 'https://global-beats.vercel.app', // Remove the trailing slash
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    optionsSuccessStatus: 204,
+  }),
+);
+
+app.get('/api/topheadlines', async (req, res) => {
+  try {
+    const response = await axios.get(requests.topHeadlines, {
+      params: { apiKey, ...req.query },
+    });
+    res.send(response.data);
+  } catch (e) {
+    res.status(500).send('There was an error fetching Top Headlines');
+  }
+});
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log('App running on port ' + process.env.PORT);
+});
